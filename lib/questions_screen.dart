726b4dea0c8f59510268a309c08f8bfcdcaf5995
Goodness Nwankwo (NDER) <126233126+NdersCode@users.noreply.github.com>
2023-06-09 09:19:51 +0100

    Latest Commit

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:app_002/answer_button.dart';
 import 'package:app_002/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,9 +13,17 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -25,9 +34,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 16,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 244, 211, 255),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
               ),
               textAlign: TextAlign.center,
             ),
@@ -36,7 +46,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               (answers) {
                 return AnswerButton(
                   answerText: answers,
-                  onTap: () {},
+                  onTap: answerQuestion,
                 );
               },
             ),
