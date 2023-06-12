e2ba752030359329fb174917c7670bc8b04c28d2
Goodness Nwankwo (NDER) <126233126+NdersCode@users.noreply.github.com>
2023-06-12 11:15:07 +0100

    Latest Commit

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,12 +1,34 @@
+import 'package:app_002/data/questions.dart';
+import 'package:app_002/questions_summary.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key, required this.chosenAnswers});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
 
   final List<String> chosenAnswers;
 
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i]
+        },
+      );
+    }
+
+    return summary;
+  }
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -15,12 +37,16 @@ class ResultsScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const Text('You answered X out of Y questions correctly!'),
-            const SizedBox(height: 30),
-            const Text('List of answers and questions...'),
-            const SizedBox(height: 30),
+            const SizedBox(
+              height: 30,
+            ),
+            QuestionsSummary(getSummaryData()),
+            const SizedBox(
+              height: 30,
+            ),
             TextButton(
               onPressed: () {},
-              child: const Text('Restart Quiz'),
+              child: const Text('Restart Quiz!'),
             )
           ],
         ),
